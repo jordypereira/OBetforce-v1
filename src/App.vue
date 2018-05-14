c47a34e51ffda2f081509d94bebf23206a4faa2f
Jordy Pereira <jordy.pereira@hotmail.com>
2018-05-14 17:34:38 +0200

    added icons

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <member-block></member-block>
         <v-list-tile value="true" v-for="(item, i) in items" :key="i" :to="item.to">
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
@@ -14,7 +14,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app dark>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
     </v-toolbar>
     <v-content>
