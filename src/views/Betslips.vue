f46c09e4f6b80a578ba34f237f2bc9d7e5eab003
Jordy Pereira <jordy.pereira@hotmail.com>
2018-06-07 02:36:25 +0200

    betslip vuetify

---
+++ src/views/Betslips.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="betslips">
+    <h1>Betslips</h1>
+    <v-layout>
+      <v-flex xs12 sm3 class="mx-auto">
+        <v-card>
+          <v-card-title>
+            <div>
+              <div class="black">
+                <h1 class="white--text">BETTING SLIP</h1>
+              </div>
+              <span class="grey--text">Number 10</span><br>
+              <span>Whitehaven Beach</span><br>
+              <span>Whitsunday Island, Whitsunday Islands</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
