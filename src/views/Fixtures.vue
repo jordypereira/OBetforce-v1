1a184d6b834b4975441a88f85d36e5e2c41d1ace
perjor <jordy.pereira@hotmail.com>
2018-07-19 12:48:34 +0200

    Country was taking wrong id: fixed with a filter

--- src/views/Fixtures.vue
+++ src/views/Fixtures.vue
@@ -2,15 +2,14 @@
   <div id="fixtures">
     <v-container>
       <h1>Fixtures</h1>
-      <p v-if="loading">Loading...</p>
-      <v-layout row wrap>
-        <v-flex v-if="countries && fixtures" xs12 sm8 v-for="(match, i) in fixtures" :key="i">
+      <p v-if="loading || fixturesLoading">Loading...</p>
+      <v-layout row wrap v-if="countries && fixtures && !fixturesLoading && !loading">
+        <v-flex xs12 sm8 v-for="(match, i) in fixtures" :key="i">
           <fixture-row :match="match" :country="getCountry(match.league.data.country_id)" />
         </v-flex>
-        <v-flex v-if="!fixtures && !loading && !countries" xs12 sm 6>No Matches Found</v-flex>
+        <v-flex v-if="!fixtures" xs12 sm 6>No Matches Found</v-flex>
       </v-layout>
     </v-container>
-
   </div>
 </template>
 
@@ -23,21 +22,19 @@ export default {
   components: {
     FixtureRow,
   },
-  data() {
-    return {
-      country: null,
-      info: null,
-    };
-  },
   computed: { ...mapState({
     fixtures: state => state.sportmonks.fixtures,
     countries: state => state.sportmonks.countries,
-    loading: state => state.shared.loading
+    loading: state => state.shared.loading,
+    fixturesLoading: state => state.sportmonks.fixturesLoading
   })},
 
   methods: {
     getCountry(id){
-      return this.countries[id]
+      const country = this.countries.filter((country) => {
+        return country.id == id
+      })
+      return country[0]
     }
   },
   created() {
