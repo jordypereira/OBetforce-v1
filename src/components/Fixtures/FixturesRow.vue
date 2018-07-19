90cf214466b0496ce4dea1d370ce85a36481da8f
perjor <jordy.pereira@hotmail.com>
2018-07-19 12:47:44 +0200

    hide when odds arent there

--- src/components/Fixtures/FixturesRow.vue
+++ src/components/Fixtures/FixturesRow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="match my-2 elevation-20 pa-2">
     <div class="match-league">
-      <img :src="`/images/countryflags/${this.country.name}.png`" alt=""> {{ country.name }} - {{ match.league.data.name }}
+      <img :src="`/images/countryflags/${this.country.name}.png`"> {{ country.name }} - {{ match.league.data.name }}
     </div>
     <v-layout text-xs-center align-center justify-center class="secondary white--text">
       <v-flex xs4 class="elevation-20 py-2">
@@ -19,7 +19,7 @@
       </v-flex>
     </v-layout>
     <h3 class="text-xs-center mt-2">Pre Match Odds</h3>
-    <v-layout text-xs-center align-center justify-center class="secondary white--text">
+    <v-layout text-xs-center align-center justify-center class="secondary white--text" v-if="match.odds.data[0]">
       <Odd name="Home" :odd="match.odds.data[0].bookmaker.data[0].odds.data[0].value" />
       <Odd name="Draw" :odd="match.odds.data[0].bookmaker.data[0].odds.data[1].value" />
       <Odd name="Away" :odd="match.odds.data[0].bookmaker.data[0].odds.data[2].value" />
