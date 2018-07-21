d8154ad03b3aeaa3066dd473d68c6b204a8aed20
perjor <jordy.pereira@hotmail.com>
2018-07-21 11:50:10 +0200

    Added single betslip row

---
+++ src/components/Betslips/BetslipRow.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout>
+    <v-flex xs2>
+      <v-btn block icon right>
+        <v-icon>close</v-icon>
+      </v-btn>
+    </v-flex>
+    <v-flex xs8>
+      <v-layout row wrap>
+        <v-flex xs12 d-flex>
+          Portugal
+        </v-flex>
+        <v-flex xs12 d-flex class="grey--text">
+          Reguliere speeltijd
+        </v-flex>
+        <v-flex xs12 d-flex>
+          Portugal - Spanje
+        </v-flex>
+      </v-layout>
+    </v-flex>
+    <v-flex xs2 d-flex align-start justify-end>
+      4.05
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
